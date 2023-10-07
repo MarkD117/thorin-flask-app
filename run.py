5a520b140acbcef7afc6887e79a6997ab368fd4b
Mark Dunne <Markdunne117@gmail.com>
2023-10-07 18:13:43 +0000

    Render company information from json file

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 # Importing flask class, capital f indicates a class name.
 # Also imports render_template function from flask
 from flask import Flask, render_template
@@ -22,17 +23,24 @@ def index():
 # html page. This function is also called the view.
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
+# Other arguents can be used to render server-code on the front end.
+# Our custom argument 'page_title' is used to set the page title on the html
+# pages. Head to the h2 element on contact.html to see how
+# this information is displayed.
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 # __main__ is the default module in python. It is the first one we run
