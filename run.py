689092efcdca032198830a5819c426a53e9782d6
Mark Dunne <Markdunne117@gmail.com>
2023-10-08 18:45:01 +0000

    Add form and flash message to display on submission

--- run.py
+++ run.py
@@ -1,13 +1,22 @@
 import os
 import json
 # Importing flask class, capital f indicates a class name.
-# Also imports render_template function from flask
-from flask import Flask, render_template
+# Also imports render_template function from flask to display
+# front end files. Imports request library to handle things like
+# finding out what method is used and it will also hold our form
+# object when we've posted it. Imports flash to display messages
+# to the user on the front end
+from flask import Flask, render_template, request, flash
+# Importing key if file exists
+if os.path.exists("env.py"):
+    import env
+
 
 # creating an instance of this and storing it in a variable called 'app'.
 # The first argument of the Flask class, is the name of the application's
 # module - our package.
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 # python decorator wraps function. The route decorator binds the funtion to
@@ -44,19 +53,25 @@ def about_member(member_name):
             if obj["url"] == member_name:
                 member = obj
     # The 1st member is the variable name being passed into the html file
-    # The 2nd member is the member object created on line 40
+    # The 2nd member is the member object created on line 44
     return render_template("member.html", member=member)
+
+
+# methods argument allows route to accept get and post methods
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        # flash displays message to the user
+        flash("Thanks {}, we have received your message!".format(
+            # curly brackets take the name value from the form
+            request.form.get("name")))
+    return render_template("contact.html", page_title="Contact")
 
 
 # Other arguents can be used to render server-code on the front end.
 # Our custom argument 'page_title' is used to set the page title on the html
-# pages. Head to the h2 element on contact.html to see how
+# pages. Head to the h2 element on careers.html to see how
 # this information is displayed.
-@app.route("/contact")
-def contact():
-    return render_template("contact.html", page_title="Contact")
-
-
 @app.route("/careers")
 def careers():
     return render_template("careers.html", page_title="Careers")
